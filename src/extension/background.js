76bceb6b658a3a987f07022e0bbb3533d02de59b
Xavi Vives <xavings@gmail.com>
2018-04-27 20:56:40 +0200

    Basic messaging

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,9 +1,8 @@
 import ReactDOM from 'react-dom'
 import { render } from 'react-dom'
 
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=>{
-    console.log('update', tabId, changeInfo, tab)
+    //console.log('update', tabId, changeInfo, tab)
     
     if(changeInfo.status==='loading')
     {
@@ -11,15 +10,65 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=>{
             file: 'js/inject.bundle.js',
         })
     }
-    
 })
 
 chrome.contextMenus.create({
-    title: "Pin text", 
+    title: "Hug this text", 
     contexts:["selection"], 
+    onclick: function(info, tab) {
+        console.log(info)
+        console.log(tab)
+        hugContent(tab.id, formatSelectedText(info) )
+    }
+})
+
+chrome.contextMenus.create({
+    title: "Hug this image", 
+    contexts:["image"], 
     onclick: function(info, tab) {
         console.log(info)
         console.log(tab)
   }
 })
 
+
+chrome.contextMenus.create({
+    title: "Hug this link    ", 
+    contexts:["link"], 
+    onclick: function(info, tab) {
+        console.log(info)
+        console.log(tab)
+  }
+})
+
+let processResponse=(response)=>
+{
+    console.log(response)
+}
+
+let sendMessage=(tabId, message)=>
+{
+    chrome.tabs.sendMessage(tabId, message, processResponse)
+}
+
+let createMessage=(functionId, args)=>
+{
+    return {
+        func:functionId,
+        args:args
+    }
+}
+
+let hugContent=(tabId, content)=>
+{
+    let msg = createMessage('inputHug', content)
+    sendMessage(tabId, msg)
+}
+
+let formatSelectedText=(info)=>
+{
+    return {
+        text:info.selectionText,
+        parents:[info.pageUrl]
+    }
+}
--- src/extension/inject.js
+++ src/extension/inject.js
@@ -2,10 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import InputDock from './inputDock'
 import {Styles} from './styles'
-import arxiuCSS from './arxiu.css';
 
 let resetStyle = '#arxiu div {\n'
-resetStyle+= 'background-color: "transparent";\n'
+resetStyle+= 'background-color: transparent;\n'
 resetStyle+= 'padding:0;\n'
 resetStyle+= 'margin:0;\n'
 resetStyle+= 'border-radius:0;\n'
@@ -25,8 +24,21 @@ let inject=(id, component)=>
 
     const injectedDiv = document.createElement('div')
     injectedDiv.id = id;
+    //injectedDiv.style.setProperty('height','100%')
     document.body.appendChild(injectedDiv);
     render(component, injectedDiv);
 }
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log('Got message', request )
+
+    sendResponse()
+});
+
+
+chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+    console.log(response);
+});
+
 
 inject('arxiu', <InputDock/>)
