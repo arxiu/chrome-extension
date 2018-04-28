37e0e8f91c5c943c5514fbe3652dc01617ba4201
Xavi Vives <xavings@gmail.com>
2018-04-28 22:29:44 +0200

    Format

--- src/extension/inputDock.js
+++ src/extension/inputDock.js
@@ -4,51 +4,53 @@ import Dock from 'react-dock'
 import { Styles } from './styles'
 import messenger from './messenger'
 
+import input from './ui/input.js'
+
 export default class InputDock extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isVisible: false,
-      dockWidth: 200
+    constructor(props) {
+        super(props)
+        this.state = {
+            isVisible: false,
+            dockWidth: 200
+        }
+        messenger.listenToBackground()
+        messenger.registerFunction('inputHug', this.inputHug)
     }
-    messenger.listenToBackground()
-    messenger.registerFunction('inputHug', this.inputHug)
-  }
 
-  inputHug = (content) => {
-    this.setState({ isVisible: true })
-  }
+    inputHug = (content) => {
+        this.setState({ isVisible: true })
+    }
 
-  buttonOnClick = () => {
-    this.setState({ isVisible: !this.state.isVisible })
-  }
+    buttonOnClick = () => {
+        this.setState({ isVisible: !this.state.isVisible })
+    }
 
-  onResize = (value) => {
-    this.setState({ dockWidth: value })
-  }
+    onResize = (value) => {
+        this.setState({ dockWidth: value })
+    }
 
-  render() {
-    return (
-      <div>
+    render() {
+        return (
+            <div>       
+                <Dock
+                    position="right"
+                    fluid={false}
+                    size={this.state.dockWidth}
+                    dimMode="none"
+                    isVisible={this.state.isVisible}
+                    dockStyle={Styles.dock}
+                    duration={200}
+                    onSizeChange={this.onResize}>
 
-        <Dock
-          position="right"
-          fluid={false}
-          size={this.state.dockWidth}
-          dimMode="none"
-          isVisible={this.state.isVisible}
-          dockStyle={Styles.dock}
-          duration={200}
-          onSizeChange={this.onResize}>
-
-          <div style={Styles.dock.body}>
-            <div style={Styles.dock.body.content}>
-              Add stuff here
+                    <div style={Styles.dock.body}>
+                        <div style={Styles.dock.body.content}>
+                            Add stuff here
+                            <input/>
+                        </div>
                     </div>
-          </div>
 
-        </Dock>
-      </div>
-    )
-  }
+                </Dock>
+            </div>
+        )
+    }
 }
