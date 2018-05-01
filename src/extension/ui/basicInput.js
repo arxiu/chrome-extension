86346f51b04b2c2a66d41ec033fb280e4f8dd3ff
Xavi Vives <xavings@gmail.com>
2018-05-01 21:18:46 +0200

    input event

--- src/extension/ui/basicInput.js
+++ src/extension/ui/basicInput.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react'
-import styled from 'styled-components';
+import { Styles } from './styles'
 
 
 export default class BasicInput extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { hasFocus: false }
     }
 
-    handleChange(event) {
+    handleChange = (event) => {
         const value = event.target.value
         this.props.onChange(value)
+        this.onFocus()
         //this.setState({ value: value });
+    }
+
+    onFocus = () => {
+        this.setState({ hasFocus: true })
+        if (this.props.onFocus)
+            this.props.onFocus()
+
+        console.log('focus')
+    }
+
+    onBlur = () => {
+        this.setState({ hasFocus: false })
+        if (this.props.onBlur)
+            this.props.onBlur()
+        console.log('blur')
+    }
+
+    onClick = ()=>
+    {
+        this.onFocus()
     }
 
     render() {
         return (
-            <div >
+            <div style={Styles.basicInput}>
 
                 Unga  bunga
                 <input
+                    style={Styles.basicInput.input}
                     type="text"
                     value={this.props.value}
-                    onChange={this.handleChange.bind(this)
-                    } />
+                    onChange={this.handleChange}
+                    onBlur={this.onBlur}
+                    onFocus={this.onFocus}
+                    onClick={this.onClick}
+                 />
             </div>
         );
     }
